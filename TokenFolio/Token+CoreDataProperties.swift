cd41b340f0757e821c0760ceba576f92e1d0a2d8
Riccardo Scanavacca <scanar01@mc-1001064.local>
2017-08-13 17:24:29 +0100

    Add detail view controller

--- TokenFolio/Token+CoreDataProperties.swift
+++ TokenFolio/Token+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  Token+CoreDataProperties.swift
 //  TokenFolio
 //
-//  Created by Riccardo Scanavacca on 04/08/2017.
+//  Created by Riccardo Scanavacca on 11/08/2017.
 //  Copyright © 2017 Riccardo Scanavacca. All rights reserved.
 //
 
@@ -20,29 +20,20 @@ extension Token {
     @NSManaged public var id: String?
     @NSManaged public var isSelected: Bool
     @NSManaged public var lastUpdated: NSDate?
-    @NSManaged public var marketCapEur: String?
-    @NSManaged public var marketCapGbp: String?
-    @NSManaged public var marketCapUsd: String?
     @NSManaged public var name: String?
     @NSManaged public var percentChange1h: String?
     @NSManaged public var percentChange7d: String?
     @NSManaged public var percentChange24h: String?
     @NSManaged public var priceBtc: NSNumber?
-    @NSManaged public var priceEur: NSNumber?
-    @NSManaged public var priceGbp: NSNumber?
-    @NSManaged public var priceUsd: NSNumber?
     @NSManaged public var quantity: NSNumber?
     @NSManaged public var rank: Int16
     @NSManaged public var symbol: String?
     @NSManaged public var totalSupply: String?
     @NSManaged public var totalValue: NSNumber?
     @NSManaged public var totalValueBtc: NSNumber?
-    @NSManaged public var totalValueEur: NSNumber?
-    @NSManaged public var totalValueGbp: NSNumber?
-    @NSManaged public var totalValueUsd: NSNumber?
-    @NSManaged public var volume24hEur: String?
-    @NSManaged public var volume24hGbp: String?
-    @NSManaged public var volume24hUsd: String?
+    @NSManaged public var marketCap: String?
+    @NSManaged public var price: NSNumber?
+    @NSManaged public var volume24h: String?
     @NSManaged public var user: User?
 
 }
